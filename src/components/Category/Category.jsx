b3db24fd2a4ba86ddec4dca1ad290fc8e521b487
saqib <maliksaqib976@gmail.com>
2024-11-23 23:56:04 +0500

    minor change in category

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -45,7 +45,7 @@ const Category = () => {
                className='w-[320px] absolute bottom-0 right-0 top-0 lg:top-[40px]'/>
             </div>
             {/* Third line */}
-            <div className='col-span-2 py-10 pl-5 bg-gradient-to-br from-primary to-primary/90 text-white rounded-3xl relative h-[320px] flex items-end'>
+            <div className='sm:col-span-2 py-10 pl-5 bg-gradient-to-br from-primary to-primary/90 text-white rounded-3xl relative h-[320px] flex items-end'>
               <div>
                 <div className='mb-4 '>
                   <p className='mb-[2px] text-white'>Enjoy</p>
